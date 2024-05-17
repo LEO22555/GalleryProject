0bf198135746db7017bc878c781bbbfc0c23ddc7
Leonardo <lemartinezba@unal.edu.co>
2024-05-16 21:00:43 -0500

    subiendo primer componente

---
+++ galleryproject/src/components/Facturas.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Facturas1 from '../images/1.png'
+
+const Facturas = () => {
+  return (
+    <div>
+        <img src="{Facturas1}" alt="factura1" />
+    </div>
+  )
+}
+
+export default Facturas
